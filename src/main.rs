109f648f0609f28c7611c506e9d12eb9e2118aec
eignnx <eignnx@gmail.com>
2019-08-28 12:38:56 -0400

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,17 @@
+use std::convert::TryFrom;
 use std::io::{self, Read};
+use std::process;
 
-fn main() -> io::Result<()> {
+use m_o::Value;
+
+fn main() {
     let mut stdin = io::stdin();
     let mut buf = String::new();
     stdin.read_to_string(&mut buf).expect("stdin read will succeed");
-    let (_, value) = m_o::parse_value(&buf).expect("input is parsable Python expression");
+    let value = Value::try_from(buf.trim()).unwrap_or_else(|e| {
+        eprintln!("Error: Could not parse input as Python data expression!");
+        eprintln!("\t{:?}", e);
+        process::exit(1);
+    });
     println!("{}", value);
-    Ok(())
 }
