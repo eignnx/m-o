851a87252dd940abd6ccc426d9ef39a611abc5aa
eignnx <eignnx@gmail.com>
2019-09-05 01:42:28 -0400

    impl to_doc for all Value variants

--- src/value/print.rs
+++ src/value/print.rs
@@ -1,43 +1,89 @@
 use std::fmt;
 
 use super::Value;
-use sparkly::{Colour, Doc, Style};
+use sparkly::{Colour, Doc, Sparkly, Style};
 
 static INDENT: usize = 4;
-static WIDTH: usize = 50;
 
 impl<'value> Value<'value> {
-    fn seq_to_doc<'iter, I>(xs: I) -> Doc
+    fn seq_to_doc<'iter, I, S>(open: &'static str, xs: I, close: &'static str) -> Doc
     where
-        I: Iterator<Item = &'iter Value<'value>>,
+        I: Iterator<Item = S>,
+        S: Sparkly,
         'value: 'iter,
     {
-        Doc::text("[", Style::default())
+        Doc::text(open, Style::default())
             .append(Doc::split_point())
             .nest(INDENT)
             .append(
                 Doc::text(",", Style::default())
                     .append(Doc::space())
-                    .join(xs.map(|value| value.to_doc()))
+                    .join(xs)
                     .nest(INDENT),
             )
             .append(Doc::split_point())
-            .append(Doc::text("]", Style::default()))
+            .append(Doc::text(close, Style::default()))
             .group()
     }
 
-    pub fn to_doc(&self) -> Doc {
+    fn dictionary_to_doc<'tmp>(pairs: &'tmp Vec<(Value<'value>, Value<'value>)>) -> Doc
+    where
+        'value: 'tmp,
+    {
+        Self::seq_to_doc(
+            "{",
+            pairs.iter().map(|(key, value)| {
+                key.to_doc()
+                    .append(Doc::text(": ", Style::default()))
+                    .append(value.to_doc())
+            }),
+            "}",
+        )
+        .group()
+    }
+
+    fn constructor_to_doc<'tmp>(
+        name: &'value str,
+        kwargs: &'tmp Vec<(&'value str, Value<'value>)>,
+    ) -> Doc
+    where
+        'value: 'tmp,
+    {
+        Doc::text(name, Style::from(Colour::Blue))
+            .append(Self::seq_to_doc(
+                "(",
+                kwargs.iter().map(|(key, value)| {
+                    Doc::text(*key, Style::from(Colour::Blue))
+                        .append(Doc::text("=", Style::default()))
+                        .append(value.to_doc())
+                }),
+                ")",
+            ))
+            .group()
+    }
+}
+
+impl<'value> Sparkly for Value<'value> {
+    fn to_doc(&self) -> Doc {
         match *self {
-            Value::Int(x) => Doc::text(x, Style::from(Colour::Green)),
+            Value::Int(x) => Doc::text(x, Style::from(Colour::Purple)),
             Value::Float(x) => Doc::text(x, Style::from(Colour::Purple)),
-            Value::List(ref xs) => Self::seq_to_doc(xs.iter()),
-            _ => unimplemented!(),
+            Value::Bool(x) => {
+                Doc::text(if x { "True" } else { "False" }, Style::from(Colour::Green))
+            }
+            Value::Symbol(x) => Doc::text(x, Style::from(Colour::Blue)),
+            Value::Str(x) => Doc::text(x, Style::from(Colour::Cyan)),
+            Value::List(ref xs) => Self::seq_to_doc("[", xs.iter().map(Self::to_doc), "]"),
+            Value::Tuple(ref xs) => Self::seq_to_doc("(", xs.iter().map(Self::to_doc), ")"),
+            Value::Set(ref xs) => Self::seq_to_doc("{", xs.iter().map(Self::to_doc), "}"),
+            Value::Dict(ref pairs) => Self::dictionary_to_doc(pairs),
+            Value::Constructor(name, ref kwargs) => Self::constructor_to_doc(name, kwargs),
         }
     }
 }
 
 impl<'a> fmt::Display for Value<'a> {
     fn fmt(&self, f: &mut fmt::Formatter) -> Result<(), fmt::Error> {
-        self.to_doc().display_opts(WIDTH, true).fmt(f)
+        self.to_doc().display().fmt(f)
     }
 }
