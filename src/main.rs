c92efb43ca8497a92277865b99b402ccb54d9475
eignnx <eignnx@gmail.com>
2019-09-06 15:33:03 -0400

    refactor main

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,39 @@
 use std::convert::TryFrom;
-use std::io::{self, Read, Write};
+use std::io::{self, Read};
 use std::process;
 
 use m_o::value::Value;
 
-fn main() {
-    let mut stdin = io::stdin();
+fn read_stdin() -> String {
     let mut buf = String::new();
-    stdin
+    io::stdin()
         .read_to_string(&mut buf)
         .expect("stdin read will succeed");
-    let value = Value::try_from(buf.trim()).unwrap_or_else(|e| {
+    buf
+}
+
+fn parse_input(input: &str) -> Value {
+    Value::try_from(input.trim()).unwrap_or_else(|e| {
         eprintln!("Error: Could not parse input as Python data expression!");
         eprintln!("\t{:?}", e);
         process::exit(1);
-    });
+    })
+}
 
-    // Get terminal width (if it's available), use 80 as default.
-    let (width, _height) = termion::terminal_size().unwrap_or_else(|_| (80, 0));
+fn terminal_width() -> usize {
+    termion::terminal_size()
+        .map(|(w, _h)| w as usize)
+        .unwrap_or(80)
+}
 
+fn pretty_print(value: Value) {
+    let width = terminal_width();
     let doc = value.to_doc();
-    let fmt = doc.pretty(width as usize);
-    writeln!(io::stdout(), "{}", fmt).expect("stdout can be written to");
+    println!("{}", doc.pretty(width));
+}
+
+fn main() {
+    let input = read_stdin();
+    let value = parse_input(&input);
+    pretty_print(value);
 }
